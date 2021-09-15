cd95669b608a74090caf856149b998f210f706e5
David <adzhamyand@gmail.com>
2021-09-15 06:05:52 +0300

    fix: fix routing for gh pages

--- src/components/whiteSpace/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./styles.module.scss";
-
-const WhiteSpace = () => {
-  return <div className={styles.white__space}></div>;
-};
-
-export default WhiteSpace;
